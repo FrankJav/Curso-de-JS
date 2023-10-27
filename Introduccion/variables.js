91c637637aa947f1aad50b8eb816eeb91ee10364
Francisco <hevifrja@gmail.com>
2023-10-27 09:54:16 -0600

    Actualizacion del vierene 27/10/23

---
+++ Introduccion/variables.js
@@ -0,0 +1,33 @@
+/* 📌 Tipos de datos que podemos guardar en las variables:
+    string - Cadena de Texto
+    number - Numero
+    boolean - Booleano (verdadero o falso)
+    object - Objeto
+    function - Funciones
+
+    null - Valor nulo
+    undefined - Valor sin definir
+*/
+
+var edad = 54;
+//console.log(edad);
+let nombre = 'Francisco';
+//nombre = ' Carlos';
+const correo = 'email@correo.com';
+
+let telefono, pais, id;
+telefono = 12121212
+//console.log(telefono);
+
+const resultado = 4 + 4;
+//console.log(resultado);
+
+const nombre1 = 'CArlos ';
+const nombre2 = 'Arturto';
+const nombreCopleto = nombre1 + nombre2;
+//console.log(nombreCopleto);
+
+let miVariable = 'texto';
+miVariable = 7;
+miVariable = true;
+
